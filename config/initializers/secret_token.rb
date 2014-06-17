8286af1e9d2bb931359cf103f61e966d77e0582c
mic <mic@mic-SATELLITE.(none)>
2014-06-17 17:50:25 +0200

    baseline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App1::Application.config.secret_token = '1772342b673ad927b3ee25425aee92ab7e26eb21455773d5918285e878b642384e48736b8a87153cb26a2d32f9285f9590fd7f648de9d3763494e60834d2af72'
